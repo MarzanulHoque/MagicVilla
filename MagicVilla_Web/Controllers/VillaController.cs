8faca0aa034807709615b0d9159186b5fed4f197
Marzanul Hoque <marzanul.haque@bizzntek.com>
2024-09-25 14:43:51 +0600

    Create Villa UI integrated

--- MagicVilla_Web/Controllers/VillaController.cs
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -29,5 +29,22 @@ namespace MagicVilla_Web.Controllers
             }
             return View(list);
         }
-    }
+		public async Task<IActionResult> CreateVilla()
+		{
+			return View();
+		}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+		public async Task<IActionResult> CreateVilla(VillaCreateDTO model)
+		{
+			if (ModelState.IsValid) {
+			    var response = await _villaService.CreateAsync<APIResponse>(model);
+                    if(response != null && response.IsSuccess)
+                    {
+				        return RedirectToAction("Index");
+                    }
+			}
+			return View(model);
+		}
+	}
 }
