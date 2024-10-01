5c4d834f0a5432e94179b378b0dbcbb6aea46ce1
Marzanul Hoque <marzanul.haque@bizzntek.com>
2024-10-01 10:33:28 +0600

    SweetAlert Added.

--- MagicVilla_Web/Controllers/VillaController.cs
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -42,10 +42,12 @@ namespace MagicVilla_Web.Controllers
 			    var response = await _villaService.CreateAsync<APIResponse>(model);
                     if(response != null && response.IsSuccess)
                     {
-				        return RedirectToAction("IndexVilla");
+                    TempData["success"] = "Villa created successfully";
+                    return RedirectToAction("IndexVilla");
                     }
 			}
-			return View(model);
+            TempData["error"] = "Error encountered.";
+            return View(model);
 		}
         public async Task<IActionResult> UpdateVilla(int villaId)
         {
@@ -70,9 +72,11 @@ namespace MagicVilla_Web.Controllers
                 var response = await _villaService.UpdateAsync<APIResponse>(model);
                 if (response != null && response.IsSuccess)
                 {
+                    TempData["success"] = "Villa updated successfully";
                     return RedirectToAction(nameof(IndexVilla));
                 }
             }
+            TempData["error"] = "Error encountered.";
             return View(model);
         }
 		public async Task<IActionResult> DeleteVilla(int villaId)
@@ -95,10 +99,11 @@ namespace MagicVilla_Web.Controllers
 				var response = await _villaService.DeleteAsync<APIResponse>(model.Id);
 				if (response != null && response.IsSuccess)
 				{
-					return RedirectToAction(nameof(IndexVilla));
+                TempData["success"] = "Villa deleted successfully";
+                return RedirectToAction(nameof(IndexVilla));
 				}
-			
-			return View(model);
+            TempData["error"] = "Error encountered.";
+            return View(model);
 		}
 	}
 }
