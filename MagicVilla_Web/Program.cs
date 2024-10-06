d8a83ea6112779b07550edb0ae1a33b8cd5b0c81
Marzanul Hoque <marzanul.haque@bizzntek.com>
2024-10-06 11:53:56 +0600

    Auth Controller Login & Logout Action Method Implemented

--- MagicVilla_Web/Program.cs
+++ MagicVilla_Web/Program.cs
@@ -16,6 +16,14 @@ builder.Services.AddScoped<IVillaNumberService, VillaNumberService>();
 
 builder.Services.AddHttpClient<IAuthService, AuthService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(100);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
@@ -31,7 +39,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseSession();
 app.UseAuthorization();
 
 app.MapControllerRoute(
